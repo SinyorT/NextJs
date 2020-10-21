4b3a00b746e27c732be58a9e306c4ba7e6a7a525
DESKTOP-1HFSSB6\CAN <ngcsoftwre@outlook.com>
2020-10-21 20:20:31 +0300

    State of functional component  #1

--- movie-app/components/sideMenu.js
+++ movie-app/components/sideMenu.js
@@ -1,6 +1,22 @@
 
+import { useState } from 'react'
 
-const sideMenu = () => {
+const SideMenu = () => {
+    const [count, setCount] = useState(0)
+
+
+    const increment = () => {
+         const newCount=count+1
+         setCount(newCount)
+    }
+
+    const decrement = () => {
+        const newCount=count-1
+        setCount(newCount)
+    }
+
+
+
     return (
         <div>
             <h1 className="my-4">Shop Name</h1>
@@ -9,8 +25,19 @@ const sideMenu = () => {
                 <a href="#" className="list-group-item">Category 2</a>
                 <a href="#" className="list-group-item">Category 3</a>
             </div>
+
+            <div>
+                {/* 2ci yol burada click de function yazmaq */}
+                <button onClick={increment} className="btn btn-primary">Increment Number</button>
+                <button onClick={decrement} className="btn btn-primary">Decrement Number</button>
+                <h1>{count}</h1>
+            </div>
         </div>
+
+
+
+
     )
 }
 
-export default sideMenu
+export default SideMenu
