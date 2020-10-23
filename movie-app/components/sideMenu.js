31e332388d15eadf060b1a2612518a2fec3a017c
DESKTOP-1HFSSB6\CAN <ngcsoftwre@outlook.com>
2020-10-23 21:15:50 +0300

    Iterate over list of data

--- movie-app/components/sideMenu.js
+++ movie-app/components/sideMenu.js
@@ -4,7 +4,6 @@ import { useState } from 'react'
 const SideMenu = (props) => {
     return (
         <div>
-            <button onClick={props.clickHandler}>Click Me!</button>
             <h1 className="my-4">{props.appName}</h1>
             <div className="list-group">
                 <a href="#" className="list-group-item">Category 1</a>
@@ -13,7 +12,6 @@ const SideMenu = (props) => {
             </div>
 
             <div> 
-                <h1>{props.count}</h1>
             </div>
         </div>
 
