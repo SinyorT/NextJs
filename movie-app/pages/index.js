4d1e706090402f9aa85c3793e2e1a0740503ff49
DESKTOP-1HFSSB6\CAN <ngcsoftwre@outlook.com>
2020-10-25 14:07:58 +0300

    Use Effect

--- movie-app/pages/index.js
+++ movie-app/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Head from 'next/head'
 import Navbar from '../components/navbar'
 import SideMenu from '../components/sideMenu'
@@ -10,10 +10,26 @@ import { getMovies } from '../actions'
 const Home = () => {
 
   const [movies,setMovies]=useState([])
+  const [count,setCount]=useState(0)
+  useEffect(()=>{
+    debugger
+  //improve this because now it's called everytime
+    // getMovies().then((movies)=>{
+    //   setMovies(movies)
+    // })
 
-    getMovies().then((movies)=>{
-      setMovies(movies)
-    })
+    const fetchData= async()=>{
+      const resMovies = await getMovies()
+      setMovies(resMovies)
+    }
+    debugger
+    fetchData()
+  },[count])
+
+
+
+
+
 
   return (<div>
     <Head>
@@ -23,10 +39,11 @@ const Home = () => {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </Head>
-
+    
     <Navbar />
     <div className="home-page">
       <div className="container">
+        <button onClick={()=>{setCount(count+1)}}>click me</button>
         <div className="row">
           <div className="col-lg-3">
             <SideMenu
