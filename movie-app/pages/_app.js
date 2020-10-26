1c33ce2c9eaac2f7413844817d4e8aec76a2ebe8
DESKTOP-1HFSSB6\CAN <ngcsoftwre@outlook.com>
2020-10-26 20:10:13 +0300

    App container duzenleme

--- movie-app/pages/_app.js
+++ movie-app/pages/_app.js
@@ -1,15 +1,39 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 import App from 'next/app'
 class MovieApp extends App {
-    render(){
-      //Component holds page you are navigating to
-      const {Component} =this.props
 
-      return(
-        <Component />
-      )
-    }
+  // TODO : execute here getInitialProps and pass  this data to your page
+
+  render() {
+    //Component holds page you are navigating to
+    const { Component } = this.props
+
+    return (
+      <div className="base-page">
+        <Head>
+          <title>Home</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </Head>
+        <Navbar />
+        <div>
+          <Component />
+        </div>
+        <Footer />
+        <style jsx>{`
+        .base-page{
+          padding-top:80px;
+        }
+    `}
+        </style>
+      </div>
+    )
+  }
 }
 
 export default MovieApp
