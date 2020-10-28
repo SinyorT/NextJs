d1dbf6ea11abdd2bcf306b7cd2bfff55a690d23a
DESKTOP-1HFSSB6\CAN <ngcsoftwre@outlook.com>
2020-10-28 11:10:48 +0300

    test

--- movie-app/pages/movies/[id].js
+++ movie-app/pages/movies/[id].js
@@ -11,8 +11,8 @@ const Movie = (props) => {
     return (
         <div className="container">
             <div class="jumbotron">
-    <h1 className="display-4">{movie.name} </h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 className="display-4">{movie.name} </h1>
+                <p className="lead">This is sasasle jumbotron-style component for calling extra attention to featured content or information.</p>
                 <hr className="my-4" />
                 <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
                 <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
