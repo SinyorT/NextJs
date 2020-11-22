25d6f3e066f5de2dfc8d9ca59d43314059a3dd7c
Sinyor <abdurahmanov.togrul@gmail.com>
2020-11-22 20:30:23 +0300

    Edit page for movie

---
+++ movie-app/pages/movies/[id]/edit.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import MovieCreateForm from '../../../components/movieCreateForm'
+import { getMovieById } from '../../../actions'
+
+
+class EditMovie extends React.Component {
+
+    static getInitialProps({ query }) {
+        return { query }
+    }
+
+    state = {
+        movie: {}
+    }
+
+
+    componentDidMount() {
+        const { id } = this.props.query
+
+        getMovieById(id).then(movie => {
+            this.setState({ movie })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <h1>Edit the Movie</h1>
+                    {JSON.stringify(this.state.movie)}
+                    <MovieCreateForm />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EditMovie
+
